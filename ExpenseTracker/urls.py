f6799f3fe94582c86e2a2e10b33cfe59debc1526
Siddhant Gupta <siddhantgupta@Siddhants-Mac-mini.local>
2025-07-25 13:27:36 +0530

    Deployment

--- ExpenseTracker/urls.py
+++ ExpenseTracker/urls.py
@@ -21,5 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('exp_tracker.urls'))
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
