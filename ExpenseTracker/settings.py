bc6ee87e123caffdca78c0ca6b95b17b2db13191
Siddhant Gupta <siddhantgupta@Siddhants-Mac-mini.local>
2025-07-26 01:23:58 +0530

    Added for render

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -81,9 +81,6 @@ WSGI_APPLICATION = 'ExpenseTracker.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
     )
 }
 
